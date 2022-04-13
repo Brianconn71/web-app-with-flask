419918a300ff87ae2aee3abb6286f5d368ee50ec
Brian Connolly <Brian.connolly71@gmail.com>
2022-04-13 18:05:28 +0100

    basic set up

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template, request, flash
 
 # creates class from app
 app = Flask(__name__)
+app.secret_key = "hooray"
 
 @app.route("/home")
 def index():
     flash("What is your name?")
-    return render_template("../index.html")
+    return render_template("index.html")
+
+
+@app.route("/greeting", methods=["POST", "GET"])
+def greeting():
+    flash("Hi " + str(request.form['name']) + ', how are ya keeping?')
+    return render_template("templates/index.html")
